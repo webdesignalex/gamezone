59619be3a104c313e36a0f51569eebbf0bf70e71
kaimtowny <niniee61@gmail.com>
2023-09-21 22:04:45 +0630

    styles added

---
+++ styles/globalStyles.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native'
+
+export const globalStyles = StyleSheet.create({
+
+    
+});
+
+export const images = {
+    rating:{
+        '1':require('../assets/rating-1.png'),
+        '2':require('../assets/rating-2.png'),
+        '3':require('../assets/rating-3.png'),
+        '4':require('../assets/rating-4.png'),
+        '5':require('../assets/rating-5.png'),
+    }
+}
