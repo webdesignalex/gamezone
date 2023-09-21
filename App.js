fce62a0069067a993a834a7cb3e205d33bf3cd6a
kaimtowny <niniee61@gmail.com>
2023-09-21 22:05:15 +0630

    App JS file added

---
+++ App.js
@@ -0,0 +1,14 @@
+import React,{useState} from 'react';
+import { NavigationContainer} from '@react-navigation/native'
+import HomeStack from './routes/HomeStack';
+
+
+
+export default function App() {
+    return(
+      <NavigationContainer>
+        <HomeStack />
+      </NavigationContainer>
+    );
+}
+
